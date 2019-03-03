7b22f49233ed5651b6188c8abc06918c918b349b
projectupto <preston.wucong@gmail.com>
2019-03-03 11:16:50 -0800

    Add files via upload

---
+++ likescript.py
@@ -0,0 +1,156 @@
+import time
+import sys
+import io
+import getpass
+import logging
+import requests
+
+from instapy import InstaPy
+from instapy import smart_run
+from instapy.util import parse_cli_args
+
+PRODUCTION = True
+
+######################################
+# configuration
+#
+# main customization
+SLEEP_BETWEEN_EACH_FOLLOW = 20
+SLEEP_BETWEEN_EACH_UNFOLLOW = 20
+SLEEP_AFTER_ALL_FOLLOW = 200
+SLEEP_AFTER_ALL_LIKE = 180
+HEADLESS_BROWSER = True
+BYPASS_SUSPICIOUS_ATTEMPT = True
+CHECK_IN_TO_SERVER = True
+REPORT_STATUS_TO_SERVER = True
+#
+# default login credentials
+DEFAULT_USERNAME = 'wendyhufox'
+DEFAULT_PASSWORD = 'jiuweihu54321'
+#
+# other arguments for instapy
+INT_MAX = sys.maxsize
+SLEEP_DELAY = 25
+UNFOLLOW_AFTER_AT_LEAST = 1
+#
+# server url to report
+SERVER = ("http://socialgrow.live" if PRODUCTION else "http://localhost:3001")
+CHECK_IN_URL = SERVER + "/admin/check-in"
+REPORT_STATUS_URL = SERVER + "/admin/report-status"
+#######################################
+
+
+# instagram follow targets data
+users_celebrity = ['justinbieber', 'taylorswift', 'selenagomez', 'kimkardashian', 'arianagrande', 'instagram',
+                   'beyonce', 'kyliejennr', 'katyperry', 'therock']
+users_test_subset = ['justinbieber', 'taylorswift']
+users_test_single = ['justinbieber']
+users = users_test_subset
+
+# backup stdout and stderr
+stdout = sys.stdout
+stderr = sys.stderr
+# set a global logger
+logger = logging.getLogger()
+
+
+# the main function
+def main():
+    # build up communication with instapy. Hack print() and logger
+    # (1) redirect stdout/stderr to customised string streams
+    # (2) for each instapy IO operation, report it's buffer to node.js server
+    #
+    class MyIO(io.StringIO):
+        def __init__(self):
+            super(MyIO, self).__init__()
+            self.report = False
+            self.reporter = None
+
+        def begin_report(self, yes_or_no):
+            self.report = yes_or_no
+
+        def set_reporter(self, r):
+            self.reporter = r
+
+        def write(self, buffer):
+            super(MyIO, self).write(buffer)
+            stdout.write(buffer)
+            if self.report and self.reporter is not None:
+                self.reporter.send(buffer)
+
+    class Reporter:
+        def __init__(self, ig_user, sys_user):
+            self.ig_user = ig_user
+            self.sys_user = sys_user
+
+        def send(self, buffer):
+            # call report_status function
+            report_status(self.ig_user, self.sys_user, buffer)
+
+    # redirect streams
+    stream = MyIO()
+    sys.stderr = stream
+    sys.stdout = MyIO()  # another stream for stdout, do not enable report on this stream
+    # sys.stdout = io.StringIO()  # or discard everything in stdout by directing to a never-use stream
+
+    # check in this process to server
+    cli_args = parse_cli_args()
+    instagram_user = cli_args.username or DEFAULT_USERNAME
+    system_user = getpass.getuser()
+    if CHECK_IN_TO_SERVER:
+        check_in(instagram_user, system_user)
+
+    # setup reporter for stderr stream
+    reporter = Reporter(instagram_user, system_user)
+    stream.set_reporter(reporter)
+    if REPORT_STATUS_TO_SERVER:
+        stream.begin_report(True)
+
+    # get an InstaPy session!
+    # set headless_browserTrue to run InstaPy in the background
+    session = InstaPy(username=DEFAULT_USERNAME,
+                      password=DEFAULT_PASSWORD,
+                      headless_browser=HEADLESS_BROWSER,
+                      bypass_suspicious_attempt=BYPASS_SUSPICIOUS_ATTEMPT)
+
+    # run the task
+    with smart_run(session):
+        while True:
+           cur = time.time()
+        
+           session.set_user_interact(amount=1, randomize=False, percentage=100, media='Photo')
+           session.like_by_tags(['Fitness', 'Gym', 'Bodybuilding', 'fit'], amount=1, interact=False)
+
+           logger.warning( time.time() - cur)
+
+           time.sleep(SLEEP_AFTER_ALL_LIKE)
+
+
+# check in to server
+def check_in(instagram_user, system_user):
+    # print("\n[check in]")
+    data = {'instagramUser': instagram_user,
+            'systemUser': system_user}
+    try:
+        requests.post(url=CHECK_IN_URL, data=data)
+    except ConnectionError:
+        pass
+
+
+# report status to server
+def report_status(instagram_user, system_user, buffer):
+    # print("[report status]\n")
+    buffer = buffer.rstrip()
+    if buffer == "":
+        return
+
+    data = {'instagramUser': instagram_user,
+            'systemUser': system_user,
+            'message': buffer}
+    try:
+        requests.post(url=REPORT_STATUS_URL, data=data)
+    except ConnectionError:
+        pass
+
+# call main function
+main()
