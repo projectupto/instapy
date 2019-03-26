b7f7d8c7616677cfec4cfadee9a316d1c2e7f118
projectupto <preston.wucong@gmail.com>
2019-03-26 16:11:29 -0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
+
 # Simple update script for Linux
 
 echo "Updating InstaPy..."
 echo "===================="
 pip install -U instapy
+
+apt-get install firefox
+git clone https://github.com/1665673/InstaGrow.git
+cd InstaGrow
+cp firefox/geckodriver.ubuntu /usr/local/bin/geckodriver
